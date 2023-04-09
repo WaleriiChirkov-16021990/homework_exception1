20884c82a92bfa7bb99b5fc0ef2d67de948cc665
Валерий Чирков <zx84058405@live.com>
2023-04-09 18:48:03 +0300

    two commit completed the first part of the task

--- src/Class_for_trow_exception.java
+++ src/Class_for_trow_exception.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.logging.Logger;
 import java.util.Scanner;
 
 /*
@@ -9,9 +9,25 @@ import java.util.Scanner;
 - Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
 */
 public class Class_for_trow_exception <T extends Integer> {
-	ArrayList<T> list;
-	int[] array = new int[10];
-	String string;
+	private ArrayList<T> list;
+	
+	public ArrayList<T> getList() {
+		return list;
+	}
+	
+	public void setList(ArrayList<T> list) {
+		this.list = list;
+	}
+	
+	public Logger getLogger() {
+		return logger;
+	}
+	
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+	
+	private Logger logger;
 
 	public static int readLocalFile(String path) throws FileNotFoundException,NumberFormatException {
 		try (Scanner scanner = new Scanner(new File(path))) {
@@ -33,7 +49,15 @@ public class Class_for_trow_exception <T extends Integer> {
 		}
 	}
 	
-	public void addArray(String s) {
+	public double divNumbers(int a, int b) throws ArithmeticException {
+		try {
+			return a / b;
+		} catch (ArithmeticException zero) {
+			System.out.println("На ноль делить нельзя !");
+			Logger logger = Logger.getAnonymousLogger();
+			logger.info("Пользователь пробует нарушить математические правила.");
+		}
+		return 0;
 	}
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println("Hello world!");
 		Class_for_trow_exception c = new Class_for_trow_exception();
+//		System.out.println(Class_for_trow_exception.readLocalFile("src/fileInt.tx"));
+//		    Строка выше попытается считать не существующий файл, и мы получим Exception +  предусмотренное сообщение об инциденте;
 //		System.out.println(Class_for_trow_exception.readLocalFile("src/fileInt.txt"));
+//          Строка выше прочитает существующий файл , в котором лежат валидные данные для программы
 //		System.out.println(Class_for_trow_exception.readLocalFile("src/fileString.txt"));
-//		c.list = new ArrayList<>();
-		c.addList(4);
-		c.array[1] = 4;
-		c.array[3] = "fge";
+//          Строка выше прочтёт существующий файл, в котором лежат не валидные данные, получаем Exception + информационное сообщение
+//		c.list = new ArrayList<>(); //
+//		c.addList(4);  //
+//		    @ 2 строки выше демонстрируют нам алгоритм по обработке 1 исключения (NullPointerException) Если не рассскомментировать строку 13, не произойдет
+//		инициализация списка, и при попытки добавления любого элемента в этот список мы получим исключение.
+		System.out.println(c.divNumbers(1,0)); // Данная мтрока предупредит пользователя о попытке деления на 0 и вернет "0"
+		System.out.println(c.divNumbers(10,5)); // А эта строчка выполнит деление, так как в ней нет арифметических ошибок и вернет результат = "2".
 		
 	}
 	
